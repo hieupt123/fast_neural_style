d626675b8d76c8f942907160390f6fe11cefb2d1
your name <pthieu2111@gmail.com>
2022-11-01 22:12:20 +0700

    fix

--- neural_style/vgg.py
+++ neural_style/vgg.py
@@ -8,7 +8,7 @@ class Vgg16(torch.nn.Module):
     def __init__(self, requires_grad=False):
         super(Vgg16, self).__init__()
         vgg_pretrained_features = models.vgg16(pretrained=True).features
-        self.maxPool = [4, 9, 18, 27, 36]
+        self.maxPool = [4, 9, 16, 23]
         for i in self.maxPool:
             vgg_pretrained_features[i] = nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
 
@@ -40,5 +40,3 @@ class Vgg16(torch.nn.Module):
         vgg_outputs = namedtuple("VggOutputs", ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3'])
         out = vgg_outputs(h_relu1_2, h_relu2_2, h_relu3_3, h_relu4_3)
         return out
-
-print(models.vgg16(pretrained=True))
